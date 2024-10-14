2c888ec0ca0285106cebce181dfe9d8263c2773b
m-vp <mvenkatprashanth1@gmail.com>
2024-10-14 09:25:51 +0530

    custom url

---
+++ models/url.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const urlSchema = mongoose.Schema({
+    shortId:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    redirectURL:{
+        type: String,
+        required: true
+    },
+    
+    visitHistory : [{timestamp : {type: Number}}]
+},
+    {timestamp:true}
+)
+
+
+module.exports = mongoose.model("URL", urlSchema);
