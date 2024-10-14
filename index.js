16adad8bf8bf2271486269b912fdfe18265dd704
m-vp <mvenkatprashanth1@gmail.com>
2024-10-14 10:31:09 +0530

    custom url

--- index.js
+++ index.js
@@ -11,21 +11,33 @@ connect()
 
 app.get('/', (req,res)=> res.end('hello'))
 app.use('/url', require('./routes/url'))
-app.use('/:shortId', async (req,res)=>{
+app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
-    const red_url = await URL.findOneAndUpdate(
-        {
-            shortId,
-        },
-        {
-            $push:{
-                visitHistory: {
-                    timestamp: Date.now(),
-                }
-            }
+
+    try {
+        const red_url = await URL.findOneAndUpdate(
+            { shortId },
+            {
+                $push: {
+                    visitHistory: {
+                        timestamp: Date.now(),
+                    },
+                },
+            },
+            { new: true } 
+        );
+
+        // Check if the URL exists
+        if (!red_url) {
+            return res.status(404).json({ error: 'URL not found' });
         }
-    )
-    res.redirect(red_url)
-})
+
+        res.redirect(red_url.redirectURL); 
+    } catch (error) {
+        console.error('Error while redirecting:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.listen(PORT, ()=> console.log('server started'))
